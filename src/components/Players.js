ca104afc02a29463bd7aefdc0bccabeca24eb146
Rich Razon <rich@carrot.co>
2019-04-06 12:50:04 -0700

    Fetching players

--- src/components/Players.js
+++ src/components/Players.js
@@ -1,10 +1,49 @@
 import React, { Component } from 'react'
+import {Route, Link} from 'react-router-dom';
+import Sidebar from './Sidebar';
+import {getPlayers} from '../api';
+import {parse} from 'query-string';
+import slug from 'slug';
 
 class Players extends Component {
+  state = {
+    players: [],
+    loading: true,
+  }
+
+  fetchPlayers = (teamId) => {
+    getPlayers(teamId)
+      .then((players) => this.setState(() => ({
+        loading: false,
+        players,
+      })))
+  }
+
+  componentDidMount() {
+    const {location} = this.props;
+    location.search
+      ? this.fetchPlayers(parse(location.search).teamId)
+      : this.fetchPlayers()
+  }
+
   render() {
+    const {players, loading} = this.state;
+    const {location, match} = this.props;
+
     return (
       <div className='container two-column'>
-        Players
+        <Sidebar
+          title='Players'
+          list={players.map(({name}) => name)}
+          loading={loading}
+          location={location}
+          match={match}
+        />
+        {
+          loading === false && location.pathname === '/players'
+            ? <div className='sidebar-instruction'>Select a Player</div>
+            : null
+        }
       </div>
     )
   }
--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom';
+import {getTeam} from '../api';
 
 class Teams extends Component {
+  componentDidMount() {
+    getTeam()
+      .then(() => {
+
+      })
+  }
+
   render() {
+
     return (
       <div className='container two-column'>
         Teams
